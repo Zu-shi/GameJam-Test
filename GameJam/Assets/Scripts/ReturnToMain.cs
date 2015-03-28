72504c7b65d90e8a28437270c9fc7103669ebe33
Trevortds <Trevortds3@gmail.com>
2015-03-28 14:05:41 -0700

    Set Scaling to work properly

---
+++ GameJam/Assets/Scripts/ReturnToMain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnToMain : MonoBehaviour {
+
+	private bool startedLoad = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetMouseButton(0)) {
+			Application.LoadLevel("MainMenu");
+			startedLoad = true;
+		}
+		
+		
+	}
+
+}
