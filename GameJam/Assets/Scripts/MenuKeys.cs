d6bc9dd3550eb8aff78450e4700a628f39618193
deshowiz <shawn.condon@yahoo.com>
2015-04-23 18:18:58 -0700

    Neutral Keys and ~ key created, also menu key sprites

---
+++ GameJam/Assets/Scripts/MenuKeys.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MenuKeys : MonoBehaviour {
+	public KeyScript keyPrefab;
+	public KeyCode newKeyCode;
+	private KeyScript newKey;
+	// Use this for initialization
+	void Start () {
+		newKey = GameObject.Instantiate(keyPrefab);
+		newKey.setSprite(newKeyCode);
+		newKey.keyCode = newKeyCode;
+		newKey.timer = 98236507265f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Give each a specific key, connect new key to planet object using mask?
+		GameObject thisPlanet = this.GetComponent <OrbitingScript> ().mask.gameObject;
+		newKey.sourcePlanet = thisPlanet.GetComponent<_Mono> ();
+		newKey.alpha = 1f;
+		Vector2 keyPosition = gameObject.GetComponent<_Mono>().xy + (-1.5f) * -2f * new Vector2 (gameObject.GetComponent<Renderer>().bounds.size.x/2, 0f) * 2f;
+		newKey.xy = keyPosition;
+		}
+}
---
+++ RollBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	void Start()
+	{
+
+
+	}
+	// For physics
+	void FixedUpdate ()
+	{
+	
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+	}
+
+}
+
