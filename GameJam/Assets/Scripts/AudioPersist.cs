76b641c4e37c15fef1090b18d6f866a94297a5e9
Trevortds <Trevortds3@gmail.com>
2015-05-08 17:02:31 -0700

    I think this should give us a mute button, press the letter m. My Unity editor is crashing though, so I'm not certain it's working

--- GameJam/Assets/Scripts/AudioPersist.cs
+++ GameJam/Assets/Scripts/AudioPersist.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class AudioPersist : MonoBehaviour {
 
+	AudioSource audio;
+
 	// Use this for initialization
 	void Start () {
 		if(GameObject.FindGameObjectsWithTag("Audio").Length > 1){
@@ -13,7 +15,14 @@ public class AudioPersist : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (Input.GetKeyDown (KeyCode.m)) {
+			if (audio.mute)
+				audio.mute = false;
+			else
+				audio.mute = true;
+		}
+		
 	}
 
 	void Awake(){
