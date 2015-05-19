f8b6495ec094196d2aedfdb64e4daf298b20d6dc
Trevortds <Trevortds3@gmail.com>
2015-05-18 18:40:56 -0700

    Options bugfix and mute: press m to mute

--- GameJam/Assets/Scripts/AudioPersist.cs
+++ GameJam/Assets/Scripts/AudioPersist.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class AudioPersist : MonoBehaviour {
 
+	AudioSource audio;
+
 	// Use this for initialization
 	void Start () {
-		if(GameObject.FindGameObjectsWithTag("Audio").Length > 1){
-			this.GetComponent<AudioSource>().Stop();
-			Destroy(this.gameObject);
-		}
+		audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.M))
+			if (audio.mute)
+				audio.mute = false;
+			else
+				audio.mute = true;
 	}
 
 	void Awake(){
