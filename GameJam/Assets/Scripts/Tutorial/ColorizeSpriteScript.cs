e265e0acba0635200f253bfb7f929a87b787f95d
Sarina White <sarinarw@gmail.com>
2015-05-27 12:37:12 -0700

    playing around with tutorial panel background; color of border can be changed via ColorizeSpriteScript (but ill change this name eventually since it's not a Sprite/SpriteRenderer being changed

---
+++ GameJam/Assets/Scripts/Tutorial/ColorizeSpriteScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class ColorizeSpriteScript : _Mono {
+
+	//public string colorString = "";
+
+	Image image;
+
+
+	// Use this for initialization
+	void Start () {
+
+		image = GetComponent<Image> ();
+		image.color = new Color (6/255f, 10/255f, 60/255f, 255/255f);
+
+
+			/*switch (colorString) {
+
+			case "":
+				image.color = Color.white;
+				break;
+
+			case "neutral":
+				image.color = Globals.PLAYER_NEUTRAL_COLOR;
+				break;
+
+			case "player1":
+				image.color = Globals.PLAYER_ONE_COLOR;
+				break;
+
+			case "player2":
+				image.color = Globals.PLAYER_TWO_COLOR;
+				break;
+			
+			default:
+				image.color = (Color)Enum.Parse(typeof(Color), colorString);
+				break;
+
+			}
+
+		image.color = new Color (image.color.r, image.color.g, image.color.b, 1f);
+		*/
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
