6047feeed2521b03b245b53d468063fd15053271
Sarina White <sarinarw@gmail.com>
2015-06-01 19:51:40 -0700

    changes some things and stuff with tutorial

--- GameJam/Assets/Scripts/Tutorial/ColorizeSpriteScript.cs
+++ GameJam/Assets/Scripts/Tutorial/ColorizeSpriteScript.cs
@@ -5,43 +5,66 @@ using System.Collections;
 
 public class ColorizeSpriteScript : _Mono {
 
-	//public string colorString = "";
-
-	Image image;
-
+	public int r, g, b, a;
+	
+	//for player1, player2, or neutral color (this is checked first)
+	public string colorString ="";
 
 	// Use this for initialization
 	void Start () {
+		
+		colorString.ToLower (); // make all chars lowercase	
+		
+		switch(colorString) {
+			
+		case "player1":
+			spriteRenderer.color = Globals.PLAYER_ONE_COLOR;
+			break;
+		case "player2":
+			spriteRenderer.color = Globals.PLAYER_TWO_COLOR;
+			break;
+		case "neutral":
+			spriteRenderer.color = Globals.PLAYER_NEUTRAL_COLOR;
+			break;
+		default:
+			spriteRenderer.color = new Color (r / 255f, g / 255f, b / 255f, a / 255f);
+			break;
+		}
 
-		image = GetComponent<Image> ();
-		image.color = new Color (6/255f, 10/255f, 60/255f, 255/255f);
-
-
+		if (a > 0) {
+			
+			float r1 = spriteRenderer.color.r;
+			float g1 = spriteRenderer.color.g;
+			float b1 = spriteRenderer.color.b;
+			
+			spriteRenderer.color = new Color (r1, g1, b1, a/255f);
+			
+		}
 			/*switch (colorString) {
 
 			case "":
-				image.color = Color.white;
+				spriteRenderer.color = Color.white;
 				break;
 
 			case "neutral":
-				image.color = Globals.PLAYER_NEUTRAL_COLOR;
+				spriteRenderer.color = Globals.PLAYER_NEUTRAL_COLOR;
 				break;
 
 			case "player1":
-				image.color = Globals.PLAYER_ONE_COLOR;
+				spriteRenderer.color = Globals.PLAYER_ONE_COLOR;
 				break;
 
 			case "player2":
-				image.color = Globals.PLAYER_TWO_COLOR;
+				spriteRenderer.color = Globals.PLAYER_TWO_COLOR;
 				break;
 			
 			default:
-				image.color = (Color)Enum.Parse(typeof(Color), colorString);
+				spriteRenderer.color = (Color)Enum.Parse(typeof(Color), colorString);
 				break;
 
 			}
 
-		image.color = new Color (image.color.r, image.color.g, image.color.b, 1f);
+		spriteRenderer.color = new Color (spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1f);
 		*/
 
 
