3e6ae6ea800ede7e95ee9d896de77be8e84af86f
Zuoming Shi <accountistaken@gmail.com>
2015-06-15 19:46:20 -0700

    rotate script fix

--- GameJam/Assets/Scripts/RotateScript.cs
+++ GameJam/Assets/Scripts/RotateScript.cs
@@ -2,17 +2,9 @@
 using System.Collections;
 
 public class RotateScript : MonoBehaviour {
-
-	float a = 0f;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		a += 2f;
 		transform.Rotate(Vector3.right * Time.deltaTime * 30);
 		//transform.rotation = Quaternion.Euler(Vector3(0, 0, a - 90));
 	}
